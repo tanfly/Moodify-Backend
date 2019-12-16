ae3372da9cbd489f98ac00ffd1c4849e50a72f17
tanfly <mcewen.stella@gmail.com>
2019-12-16 10:44:39 -0600

    added model associations and validations

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,2 +1,4 @@
 class Mood < ApplicationRecord
+    belongs_to :user
+    has_one :photo, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :moods, dependent: :destroy
+    validates_presence_of :email, :name
+    validates_uniqueness_of :email, :name
 end

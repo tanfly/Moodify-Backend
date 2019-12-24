70279814e1eccacef4acb5eb204b5d855073e77a
tanfly <mcewen.stella@gmail.com>
2019-12-23 20:55:10 -0600

    stubbing out controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,27 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  wrap_parameters :user, include: [:name, :email, :password, :photo]
+  before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
-    @users = User.all
+    users = User.all 
+        options = {
+            include: [:moods]
+        }
+        render json: UserSerializer.new(users, options)
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
+    options = {
+      include: [:moods]
+  }
+  render json: UserSerializer.new(@user, options)
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    user = User.new(user_params)
+        if user.save
+          render json: UserSerializer.new(user)
+        end
   end
 
   # PATCH/PUT /users/1
